df1fd9d0e3234483129ead37b5f12cb195adfeb7
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-10-09 17:35:27 +0530

    --- Auto-Git Commit ---

--- write.js
+++ write.js
@@ -2,5 +2,9 @@ const fs=require("fs");
 const path=require('path')
 
 
-fs.writeFile('write.txt','this is new file')
+// fs.writeFile('write.txt','this is new file')
 
+
+
+console.log(__dirname);
+console.log(__filename);
